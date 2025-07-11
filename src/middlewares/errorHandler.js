83cd9b809410d8131bb438083538c7e5747f0402
Vladyslav Avrashov <vladisloveavrashov@gmail.com>
2025-07-11 23:58:53 +0300

    fix errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,18 @@
-export const errorHandler = (error, req, res, next) => {
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
+
   res.status(500).json({
     status: 500,
     message: 'Something went wrong',
-    data: error.message || 'Unknown error',
+    data: err.message,
   });
 };
